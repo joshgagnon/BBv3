e1f04d71c8bcefae25199fa8b6db9e65151bb054
Josh Gagnon <josh.n.gagnon@gmail.com>
2020-01-15 13:29:17 +1100

    better bed description

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -69,7 +69,7 @@ Check availability &amp; make a booking
 </section>
 
 <section id="main-menu">
-<div class="container">
+<div class="container-fluid">
 
 
 	<div id="links" >
@@ -119,7 +119,7 @@ loadjscssfile('https://media.datahc.com/ratinghotel/stellar2/styles.css?v' + ran
 
 
 
-
+<div style="zoom: 0.7;" id="TA_certificateOfExcellence33" class="TA_certificateOfExcellence"><ul id="mzUuH8s6YqJ" class="TA_links taZUHe"><li id="SYtXjoL" class="wolrUj"><a target="_blank" href="https://www.tripadvisor.co.nz/Hotel_Review-g8099604-d7986014-Reviews-Redvale_Ridge_B_B-Redwood_Valley_Nelson_Tasman_Region_South_Island.html"><img src="https://www.tripadvisor.co.nz/img/cdsi/img2/awards/CoE2017_WidgetAsset-14348-2.png" alt="TripAdvisor" class="widCOEImg" id="CDSWIDCOELOGO"/></a></li></ul></div><script async src="https://www.jscache.com/wejs?wtype=certificateOfExcellence&amp;uniq=33&amp;locationId=7986014&amp;lang=en_NZ&amp;year=2019&amp;display_version=2" data-loadtrk onload="this.loadtrk=true"></script>
 
 </div>
 </section>
@@ -233,7 +233,7 @@ our local area and further afield. We love music and go to concerts when we can.
 
           </div>
 
-           <p class="text-center">Beds represent availability of the Red and Green rooms.</p>
+           <p class="text-center">The Red and Green bed icons represent availability of the Red and Green rooms.</p>
 
 
           <div class="form-row">
